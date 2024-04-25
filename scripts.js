7bc43c73b760b40d69be0962d85a2df1e5cf83a8
CarlosDev126 <carlosirisma3@gmail.com>
2024-04-24 19:53:45 -0500

    update new

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
-const modoOscuroBtn = document.getElementById('modoOscuroBtn');
-
-modoOscuroBtn.addEventListener('click', () => {
-    document.body.classList.toggle('modo-oscuro');
-});
+document.getElementById("inicio-btn").addEventListener("click", function() {
+    // Aquí puedes agregar la lógica para iniciar sesión
+    console.log("Iniciando sesión...");
+  });
+  
+  document.getElementById("registrar-btn").addEventListener("click", function() {
+    document.getElementById("opciones-registro").style.display = "block";
+    document.getElementById("ingresar-btn").style.display = "block";
+  });
+  
+  document.getElementById("generar-contraseña").addEventListener("click", function() {
+    // Generar contraseña al azar
+    var caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var contraseña = "";
+    for (var i = 0; i < 8; i++) {
+      contraseña += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    }
+    document.getElementById("contraseña").value = contraseña;
+  });
+  
